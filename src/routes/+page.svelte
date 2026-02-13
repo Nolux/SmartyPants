0823598bad7b79c4532a74813bf8a22a8c952784
Atle Selbek <atle.selbek@gmail.com>
2026-02-13 22:38:35 +0100

    Inital

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+</script>
+
+<div>
+  <h1>Hello World</h1>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    background: #ffe4d6;
+    font-family:
+      "SF Pro Rounded",
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      system-ui,
+      sans-serif;
+  }
+  div {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  h1 {
+    font-size: 48px;
+    font-weight: 600;
+    color: #3d3d3d;
+  }
+</style>
